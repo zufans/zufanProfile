6aec3c2b8ca846568e6de3cd4bb87553b89713ef
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-05-24 23:12:00 -0400

    add sh files auto script

--- setUpGitHubPages.sh
+++ setUpGitHubPages.sh
@@ -1,6 +1,2 @@
-npm i -D gh-pages
-
-# https://zufan-interfaceing-development.github.io/zufanProfile
-# "deploy": "gh-pages -d dist"
-# "preDeploy": "npm run build"
-
+react-scripts build
+gh-pages -d build
