c5e18641d97ddb91cf24fb3a03cfbf5bf6ef6c1e
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-05-24 22:56:35 -0400

    init gh-pages

---
+++ gitRip.sh
@@ -0,0 +1,10 @@
+echo "Comment Message"
+read MESSAGE
+git add .
+git status
+git commit -m "$MESSAGE"
+git status
+git branch -M main
+git status
+git push -u origin main
+git status
---
+++ setUpGitHubPages.sh
@@ -0,0 +1,6 @@
+npm i -D gh-pages
+
+# https://zufan-interfaceing-development.github.io/zufanProfile
+# "deploy": "gh-pages -d dist"
+# "preDeploy": "npm run build"
+
